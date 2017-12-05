4254d0bf2dfcecda6f35e93a8e5e429662eda883
Grant Glidewell <ggcadc@gmail.com>
2017-12-05 09:59:41 -0800

    updated

--- src/index.js
+++ src/index.js
@@ -4,14 +4,6 @@ import slides from './slides';
 const { html, htmlCollection } = new Context();
 const slidePlane = document.getElementById('container');
 
-render(html`
-<div class="slide">
-  <h1 class="title">Generators</h1>
-  <div class="content">
-    Grant Glidewell
-  </div>
-</div>`, slidePlane);
-
 const renderSlide = ({ title, text, img, items }) => {
   render(html`
   <div class="slide">
@@ -35,6 +27,19 @@ const stepGen = function* stepGen(steps) {
 };
 
 const gen = stepGen(slides);
+
+const myInfo = {
+  title: 'Generators - Awesome?',
+  text: null,
+  items: [
+    'Grant Glidewell',
+    'gglidewell.com',
+    'github.com/ggcadc',
+    'this.prototype.grant@gmail.com',
+  ],
+};
+
+renderSlide(myInfo);
 
 document.addEventListener('keypress', (e) => {
   if (e.key === 'Enter') {
